6d0ec49722691f4ace8827a9d133f0aab47cb99d
Bailey Blackstock <140233889+BaileyBlackstock@users.noreply.github.com>
2024-06-08 17:40:17 +1000

    Created initial server files

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.port || 8000;
+
+const app = express();
+
+// static folder
+app.use(express.static(path.join(__dirname,"public")));
+
+// Config ejs
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { loggedIn: false,
+        username: "balcktack",
+        items: [{name:"couch",suburb:"mudgeeraba"},
+        {name:"car",suburb:"coomera"},
+        {name:"dog",suburb:"tugan"},
+        {name:"cat",suburb:"surfers paradise"},
+        {name:"man",suburb:"miami"},
+        {name:"toyota",suburb:"palm beach"},
+        {name:"yatch",suburb:"currumbin"},
+        {name:"dirt",suburb:"nerang"}],
+        categories: [{name:"type", show:false, items:["All", "cards", "board games", "video games", "memorabilia"]},
+        {name:"price", show:false, items:["All", "<$20", "<$50", "<$200", "<$100", "<$250", "<$1000"]},
+        {name:"distance", show:false, items:["All", "<1km", "<3km", "<5km","<10km","<25km","<50km","<100km"]}]
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is Running on port ${port}.`)
+});
