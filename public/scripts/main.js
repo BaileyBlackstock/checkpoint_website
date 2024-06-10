afe23ae3ce58ec239e6a374efc10601207d4a16d
BaileyBlackstock <baileyblackstock02@gmail.com>
2024-06-10 15:39:31 +1000

    made category bar expand when clicked

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,30 @@
-const buttons = document.querySelectorAll(".category-item");
+const buttons = document.querySelectorAll(".category-button");
+const categoryBar = document.querySelector("#category-bar"); 
+
+const categories = {"type":false,"price":false,"distance":false};
+
+function updateCategoryDisplay(){
+    Object.keys(categories).forEach(category => {
+        const div = document.querySelector("#"+category);
+
+        // change expand image display option
+        const buttonChildren = div.children[0].children;
+        console.log(buttonChildren);
+        if(categories[category]){ buttonChildren[0].style.display = "none"; buttonChildren[1].style.display = "inline-block"; }
+        else {buttonChildren[1].style.display = "none"; buttonChildren[0].style.display = "inline-block";}
+
+        // change items display options
+        for(var i = 1; i < div.children.length; i++){
+            if(categories[category]){ div.children[i].style.display = "inline-block"; }
+            else {div.children[i].style.display = "none";}
+        }
+    });
+}
 
 buttons.forEach(b => {
     b.addEventListener("click", (e) =>{
-        console.log(b.name);
+        if(categories[b.name]) categories[b.name] = false;
+        else categories[b.name] = true; 
+        updateCategoryDisplay();
     });
 });
--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get('/', (req, res) => {
         {name:"toyota",suburb:"palm beach"},
         {name:"yatch",suburb:"currumbin"},
         {name:"dirt",suburb:"nerang"}],
-        categories: [{name:"type", show:false, items:["All", "cards", "board games", "video games", "memorabilia"]},
-        {name:"price", show:false, items:["All", "<$20", "<$50", "<$200", "<$100", "<$250", "<$1000"]},
-        {name:"distance", show:false, items:["All", "<1km", "<3km", "<5km","<10km","<25km","<50km","<100km"]}]
+        categories: [{name:"type", items:["All", "cards", "board games", "video games", "memorabilia"]},
+        {name:"price", items:["All", "<$20", "<$50", "<$200", "<$100", "<$250", "<$1000"]},
+        {name:"distance", items:["All", "<1km", "<3km", "<5km","<10km","<25km","<50km","<100km"]}]
     });
 });
 
